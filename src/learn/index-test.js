81211b8b7e9d7b2e3ec4518616705a25bdb5732a
iapyang <yklorse@gmail.com>
2017-11-13 16:46:00 +0800

    update

--- src/learn/index-test.js
+++ src/learn/index-test.js
@@ -2,7 +2,16 @@ const babel = require('babel-core');
 const jsx = require('babel-plugin-transform-react-jsx');
 const plugin = require('./index.js');
 
-var example = `const a = <label><button></button><tree></tree></label>`;
+var example = `
+    const a = 
+        <label>
+            <button></button>
+            <tree>
+                <label>1234455</label>
+            </tree>
+            <button>123</button>
+        </label>
+`;
 
 const {code} = babel.transform(example, {plugins: [jsx, plugin]});
 
